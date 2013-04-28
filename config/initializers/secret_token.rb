422c003430d3e13ca63879741e4f99d9461f04fc
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-04-28 23:54:44 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmechanBox::Application.config.secret_key_base = 'b79ec21ae31a88a087c961c71a94745f359f58ecd3c57f93342d1bd7684dd530c29a84d9b549831823e8465ac8dcbd88287a95e3155fe5b2e22dc48a3ab24cae'
