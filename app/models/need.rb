605d9a7a9f47eef8312cb660006645fcdf362c8e
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-05-06 12:33:17 +0900

    implement Need::STATE and IMPORTANCE

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,4 +1,7 @@
 class Need < ActiveRecord::Base
   belongs_to :staff
   has_many :answers
+
+  STATE      = ["waiting", "on_handling", "on_hold", "resolved", "rejection"]
+  IMPORTANCE = ["low_level", "middle_level", "high_level"]
 end
