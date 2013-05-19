7529da56ea8f1ed176b5ecf3c075f29740aad3ee
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-05-19 19:55:35 +0900

    add dependent destroy in models/need

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,6 +1,6 @@
 class Need < ActiveRecord::Base
   belongs_to :staff
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   STATE      = ["waiting", "on_handling", "on_hold", "resolved", "rejection"]
   IMPORTANCE = ["low_level", "middle_level", "high_level"]
