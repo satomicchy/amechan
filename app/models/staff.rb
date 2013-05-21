05c55f54883e3634518708edda84a82e27c4a040
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-05-21 12:16:18 +0900

    change sign_in page layout

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,7 +2,6 @@ class Staff < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
   has_many :needs
 end
