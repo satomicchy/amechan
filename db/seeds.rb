ed1d17872116d0e1e9ad6d70c25d9c31a928ccef
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-05-23 12:29:57 +0900

    update for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'csv'
+
+Staff.find_or_create_by_name(name: 'admin', email: 'osaka@arc-academy.co.jp', password: 'passwordosaka', password_confirmation: 'passwordosaka')
+
+staffs = CSV.open('db/staff_list.csv')
+staffs.each do |staff|
+  Staff.find_or_create_by_name(name: staff[0], email: staff[1], password: 'passwordosaka', password_confirmation: 'passwordosaka')
+end
+
+needs = CSV.open('db/osaka_requests.csv')
+needs.each do |need|
+  staff_id = Staff.where("name like ?", "%" + need[1] + "%").first.id
+  created_need = Need.find_or_create_by_content(url: need[2], content: need[3], title: need[4], staff_id: staff_id)
+  created_need.update_attributes(created_at: need[0])
+end
