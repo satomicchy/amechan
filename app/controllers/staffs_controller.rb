7bb16e3ce3aa365c6985c3b83790a38d6f3ca2a6
satomicchy <satomi_t_sora@yahoo.co.jp>
2013-05-23 15:16:33 +0900

    implement staffs_controller and views

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,4 +1,5 @@
 class StaffsController < ApplicationController
+  before_action :only_admin
   before_action :set_staff, only: [:show, :edit, :update, :destroy]
 
   # GET /staffs
@@ -70,5 +71,11 @@ class StaffsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def staff_params
       params.require(:staff).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def only_admin
+      if current_staff.id != 1
+        redirect_to root_path
+      end
     end
 end
