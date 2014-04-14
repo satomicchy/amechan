f21794c00574aac64303cfc0d116ce2c1e3b9dd1
satomicchy <satomi_t_sora@yahoo.co.jp>
2014-04-14 14:59:29 +0900

    implement Notice.run to check create/update needs

--- app/mailers/notice.rb
+++ app/mailers/notice.rb
@@ -2,6 +2,13 @@
 class Notice < ActionMailer::Base
   default from: "amechan_box_no_reply@example.com"
 
+  def self.run
+    @needs = Need.where(updated_at: (time_now - 1.day)..time_now)
+    if @needs.present?
+      Notice.update.deliver
+    end
+  end
+
   def update
     time_now = Time.now
     @needs = Need.where(updated_at: (time_now - 1.day)..time_now)
