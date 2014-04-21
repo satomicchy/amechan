8b63c83ebadfd7c7c4bcc74278acc964eeae1fdf
satomicchy <satomi_t_sora@yahoo.co.jp>
2014-04-21 10:37:54 +0900

    add comments to send mail

--- app/mailers/notice.rb
+++ app/mailers/notice.rb
@@ -3,15 +3,18 @@ class Notice < ActionMailer::Base
   default from: "amechan_box_no_reply@example.com"
 
   def self.run
-    @needs = Need.where(updated_at: (time_now - 1.day)..time_now)
-    if @needs.present?
+    time_now = Time.now
+    @needs    = Need.where(updated_at: (time_now - 1.day)..time_now)
+    @comments = Comment.where(updated_at: (time_now - 1.day)..time_now)
+    if @needs.present? || @comments.present?
       Notice.update.deliver
     end
   end
 
   def update
     time_now = Time.now
-    @needs = Need.where(updated_at: (time_now - 1.day)..time_now)
+    @needs    = Need.where(updated_at: (time_now - 1.day)..time_now)
+    @comments = Comment.where(updated_at: (time_now - 1.day)..time_now)
 
     mail to: Staff.pluck(:email), subject: "Amechan box更新のお知らせ。"
   end
