4d4eb3eb3dd303068ce3250f29b43a8109fc4cc1
satomicchy <satomi_t_sora@yahoo.co.jp>
2015-02-04 22:40:49 +0900

    change layouts around log_in log_out and application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_staff!
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_staff_session_path
+  end
 end
